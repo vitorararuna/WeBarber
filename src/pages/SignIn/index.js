81f8b70f32fce0965a721d9d50695452b3a13f6d
vitorararuna <contato.vitorararuna@gmail.com>
2020-04-03 18:25:28 -0300

    estilização na autenticação + Unform

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import logo from '~/assets/logo.svg';
 
-export default function SigniIn() {
+export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
-    <div>
-      Login
-    </div>
-  )
+    <>
+      <img src={logo} alt="GoBarber" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar sua conta gratuita</Link>
+      </Form>
+    </>
+  );
 }
