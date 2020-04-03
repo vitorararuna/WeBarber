eec254583e87ba77888483803c6f8413acd51efc
vitorararuna <contato.vitorararuna@gmail.com>
2020-04-03 19:33:40 -0300

    Configurando store (redux)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
 import { Router } from 'react-router-dom';
 
@@ -7,15 +8,19 @@ import './config/ReactotronConfig';
 import Routes from './routes';
 import history from './services/history';
 
-import GlobalStyle from './styles/global'; 
+import store from './store';
+
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
     // history: agora o Router passa a ouvir todas as partes de navegação que a gente fizer dentro desse history
-    <Router history={history}>  
-      <Routes />
-      <GlobalStyle />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyle />
+      </Router>
+    </Provider>
   );
 }
 
