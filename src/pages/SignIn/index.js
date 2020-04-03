b2ac6ac1264cb7acc6f0b3e9ef74a25dd11dad9d
vitorararuna <contato.vitorararuna@gmail.com>
2020-04-03 18:42:04 -0300

    validação com Yup

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import logo from '~/assets/logo.svg';
+import * as Yup from 'yup';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido!') //precisa estar no formato de email, se não for, vai mostrar tal mensagem
+    .required('O e-mail é obrigatório!'),
+  password: Yup.string().required('A senha é obrigatória!'),
+});
+
 
 export default function SignIn() {
   function handleSubmit(data) {
@@ -11,7 +20,7 @@ export default function SignIn() {
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input
           name="password"
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import logo from '~/assets/logo.svg';
+import * as Yup from 'yup';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é Obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido!') //precisa estar no formato de email, se não for, vai mostrar tal mensagem
+    .required('O e-mail é obrigatório!'),
+  password: Yup.string().min(6, 'Mínimo 6 caracteres').required('A senha é obrigatória!'),
+});
+
 
 export default function SignUp() {
   function handleSubmit(data) {
@@ -11,7 +21,7 @@ export default function SignUp() {
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome Completo" />
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input
