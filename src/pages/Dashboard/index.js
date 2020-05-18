9391c8fa0a8a8db60ed89d7790fde047d455d120
vitorararuna <contato.vitorararuna@gmail.com>
2020-05-18 11:45:38 -0300

    Update dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,17 +1,71 @@
-import React, { useState, useMemo } from 'react';
-import { format, subDays, addDays } from 'date-fns';
-import pt from 'date-fns/locale/pt';
-import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
-import api from '~/services/api';
-import { Container, Time } from './styles';
+import React, { useState, useMemo, useEffect } from "react";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import {
+  format,
+  subDays,
+  addDays,
+  setHours,
+  setMinutes,
+  setSeconds,
+  setMilliseconds,
+  isBefore,
+  isEqual,
+  parseISO
+} from "date-fns";
+import { utcToZonedTime } from "date-fns-tz";
+import pt from "date-fns/locale/pt";
+import api from '../../services/api';
+
+import { Container, Time } from "./styles";
+
+const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
 export default function Dashboard() {
   const [date, setDate] = useState(new Date());
+  const [schedule, setSchedule] = useState([]);
 
   const dateFormatted = useMemo(
     () => format(date, "d 'de' MMMM", { locale: pt }),
     [date]
   );
+
+  useEffect(() => {
+    async function loadSchedule() {
+      const response = await api.get("/schedule", {
+        params: { date }
+      });
+
+      // pegando qual o padrao de data do usuario
+      const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+      const data = range.map(hour => {
+        // pegando a data atual e
+        // zerando os segundos e minutos pq os appointments só podem ser
+        // de hora em hora
+        const checkDate = setMilliseconds(
+          setSeconds(setMinutes(setHours(date, hour), 0), 0),
+          0
+        );
+        // transformando a checkdate acima para o formato global, pois
+        // a hora pode variar de usuario para usuario, e é bom manter o padrao
+        // global
+        const compareDate = utcToZonedTime(checkDate, timezone);
+
+        // retornando a hora, se ela já passou sim ou não, e se existe
+        // appointments para o dia de hoje
+        return {
+          time: `${hour}:00h`,
+          past: isBefore(compareDate, new Date()),
+          appointment: response.data.find(a =>
+            isEqual(parseISO(a.date), compareDate)
+          )
+        };
+      });
+      setSchedule(data);
+    }
+
+    loadSchedule();
+  }, [date]);
 
   function handlePrevDay() {
     setDate(subDays(date, 1));
@@ -24,40 +78,24 @@ export default function Dashboard() {
   return (
     <Container>
       <header>
-        <button type="button">
-          <MdChevronLeft size={36} color="FFF" onClick={handlePrevDay} />
+        <button onClick={handlePrevDay} type="button">
+          <MdChevronLeft size={36} color="#fff" />
         </button>
         <strong>{dateFormatted}</strong>
-        <button type="button">
-          <MdChevronRight size={36} color="FFF" onClick={handleNextDay} />
+        <button onClick={handleNextDay} type="button">
+          <MdChevronRight size={36} color="#fff" />
         </button>
       </header>
 
       <ul>
-        <Time past>
-          <strong>08:00</strong>
-          <span>Thiago Martins</span>
-        </Time>
-        <Time available>
-          <strong>09:00</strong>
-          <span>Em aberto</span>
-        </Time>
-        <Time available>
-          <strong>10:00</strong>
-          <span>Em aberto</span>
-        </Time>
-        <Time available>
-          <strong>11:00</strong>
-          <span>Em aberto</span>
-        </Time>
-        <Time past>
-          <strong>12:00</strong>
-          <span>Thiago Martins</span>
-        </Time>
-        <Time past>
-          <strong>13:00</strong>
-          <span>Thiago Martins</span>
-        </Time>
+        {schedule.map(time => (
+          <Time key={time.time} past={time.past} available={!time.appointment}>
+            <strong>{time.time}</strong>
+            <span>
+              {time.appointment ? time.appointment.user.name : "Em aberto"}
+            </span>
+          </Time>
+        ))}
       </ul>
     </Container>
   );
